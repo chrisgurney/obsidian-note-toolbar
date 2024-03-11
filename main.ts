43ceb4da267ba41a5c7c264907003699c02286a0
Chris Gurney <chris@chrisgurney.ca>
2024-03-11 15:25:13 -0400

    feat: toolbar only present if name matches

--- main.ts
+++ main.ts
@@ -44,14 +44,14 @@ export default class MyPlugin extends Plugin {
 		this.app.metadataCache.on("changed", this.metadata_cache_listener);
 
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-		console.log('LOADED');
+		// console.log('LOADED');
 
 	}
 
 	onunload() {
 		this.app.workspace.off('file-open', this.file_open_listener);
 		this.app.metadataCache.off('changed', this.metadata_cache_listener);
-		console.log('UNLOADED');
+		// console.log('UNLOADED');
 	}
 
 	async load_settings() {
@@ -89,10 +89,10 @@ export default class MyPlugin extends Plugin {
 			console.log('file-open: ' + active_file.name);
 			let frontmatter = this.app.metadataCache.getFileCache(active_file)?.frontmatter
 			// console.log('- frontmatter: ' + frontmatter);
-			const notetoolbar_prop = frontmatter?.notetoolbar ?? null;
+			const notetoolbar_prop: string[] = frontmatter?.notetoolbar ?? null;
 			if (notetoolbar_prop !== null) {
 				console.log('- notetoolbar: ' + notetoolbar_prop);
-				this.render_toolbar();
+				this.render_toolbar(notetoolbar_prop);
 			}
 			else {
 				this.remove_toolbar();
@@ -103,14 +103,14 @@ export default class MyPlugin extends Plugin {
 	metadata_cache_listener = (file: TFile, data: any, cache: CachedMetadata) => {
 		console.log("metadata-changed: " + file.name);
 		// check if there's metadata we're interested in, then...
-		const notetoolbar_prop = cache.frontmatter?.notetoolbar ?? null;
+		const notetoolbar_prop: string[] = cache.frontmatter?.notetoolbar ?? null;
 		if (notetoolbar_prop !== null) {
 			// check if we're in the active file
 			let active_file = this.app.workspace.getActiveFile()?.name;
 			if (file.name === active_file) {
 				// FIXME? this also triggers if *any* metadata changes
 				console.log('- notetoolbar: ' + notetoolbar_prop);
-				this.render_toolbar();
+				this.render_toolbar(notetoolbar_prop);
 			}
 		}
 		else {
@@ -118,62 +118,81 @@ export default class MyPlugin extends Plugin {
 		}
 	}
 
-	async render_toolbar() {
+	async render_toolbar(notetoolbar_prop: string[] | null = null) {
 
-		// check if we already added the toolbar
+		console.log('1: ' + notetoolbar_prop + " is type " + typeof notetoolbar_prop);
 		let existing_toolbar = document.querySelector('.workspace-tab-container > .mod-active .dv-cg-note-toolbar');
-		if (existing_toolbar == null) {
 
-			/* create the unordered list of menu items */
-			let note_toolbar_ul = document.createElement("ul");
-			note_toolbar_ul.setAttribute("role", "menu");
-			// toolbar_items.map(function(data) {
-			this.settings.toolbar.map((item: ToolbarItem) => {
-	
-				let toolbar_item = document.createElement("a");
-				toolbar_item.className = "external-link";
-				toolbar_item.setAttribute("href", item.url);
-				// toolbar_item.setAttribute("data-index", item.index.toString());
-				// data.append_date ? toolbar_item.setAttribute("data-append-date", "true") : false;
-				// data.remove_toolbar ? toolbar_item.setAttribute("data-remove-toolbar", "true") : false;
-				toolbar_item.setAttribute("data-tooltip-position", "top");
-				toolbar_item.setAttribute("aria-label", item.tooltip);
-				toolbar_item.setAttribute("rel", "noopener");
-				toolbar_item.onclick = (e) => this.toolbar_click_handler(e);
-				toolbar_item.innerHTML = item.label;
-	
-				let note_toolbar_li = document.createElement("li");
-				item.hide_on_mobile ? note_toolbar_li.className = "hide-on-mobile" : false;
-				item.hide_on_desktop ? note_toolbar_li.className += "hide-on-desktop" : false;
-				item.hide_on_desktop ? note_toolbar_li.style.display = "none" : false;
-				note_toolbar_li.append(toolbar_item);
-	
-				note_toolbar_ul.appendChild(note_toolbar_li);
-			});
-	
-			let note_toolbar_callout_content = document.createElement("div");
-			note_toolbar_callout_content.className = "callout-content";
-			note_toolbar_callout_content.append(note_toolbar_ul);
-	
-			let note_toolbar_callout = document.createElement("div");
-			note_toolbar_callout.className = "callout dv-cg-note-toolbar";
-			note_toolbar_callout.setAttribute("tabindex", "0");
-			note_toolbar_callout.setAttribute("data-callout", "note-toolbar");
-			note_toolbar_callout.setAttribute("data-callout-metadata", "border-even-sticky");
-			note_toolbar_callout.setAttribute("data-updated", this.settings.updated);
-			note_toolbar_callout.append(note_toolbar_callout_content);
-	
-			/* workaround to emulate callout-in-content structure, to use same sticky css */
-			let div = document.createElement("div");
-			div.append(note_toolbar_callout);
-			let embed_block = document.createElement("div");
-			embed_block.className = "cm-embed-block cm-callout";
-			embed_block.append(div);
-	
-			/* inject it between the properties and content divs */
-			let properties_container = document.querySelector('.workspace-tab-container > .mod-active .metadata-container');
-			properties_container?.insertAdjacentElement("afterend", embed_block);
-	
+		// check if name matches something in prop
+		if (notetoolbar_prop == null) {
+			// if prop is null, get the prop
+			let active_file = this.app.workspace.getActiveFile();
+			if (active_file != null) {
+				let frontmatter = this.app.metadataCache.getFileCache(active_file)?.frontmatter
+				notetoolbar_prop = frontmatter?.notetoolbar ?? null;
+				console.log('2: ' + notetoolbar_prop + " is type " + typeof notetoolbar_prop);
+			}
+		}
+
+		if (notetoolbar_prop && notetoolbar_prop.includes(this.settings.name)) {
+			console.log("- OK ");
+			// check if we already added the toolbar
+			if (existing_toolbar == null) {
+
+				/* create the unordered list of menu items */
+				let note_toolbar_ul = document.createElement("ul");
+				note_toolbar_ul.setAttribute("role", "menu");
+				// toolbar_items.map(function(data) {
+				this.settings.toolbar.map((item: ToolbarItem) => {
+		
+					let toolbar_item = document.createElement("a");
+					toolbar_item.className = "external-link";
+					toolbar_item.setAttribute("href", item.url);
+					// toolbar_item.setAttribute("data-index", item.index.toString());
+					// data.append_date ? toolbar_item.setAttribute("data-append-date", "true") : false;
+					// data.remove_toolbar ? toolbar_item.setAttribute("data-remove-toolbar", "true") : false;
+					toolbar_item.setAttribute("data-tooltip-position", "top");
+					toolbar_item.setAttribute("aria-label", item.tooltip);
+					toolbar_item.setAttribute("rel", "noopener");
+					toolbar_item.onclick = (e) => this.toolbar_click_handler(e);
+					toolbar_item.innerHTML = item.label;
+		
+					let note_toolbar_li = document.createElement("li");
+					item.hide_on_mobile ? note_toolbar_li.className = "hide-on-mobile" : false;
+					item.hide_on_desktop ? note_toolbar_li.className += "hide-on-desktop" : false;
+					item.hide_on_desktop ? note_toolbar_li.style.display = "none" : false;
+					note_toolbar_li.append(toolbar_item);
+		
+					note_toolbar_ul.appendChild(note_toolbar_li);
+				});
+		
+				let note_toolbar_callout_content = document.createElement("div");
+				note_toolbar_callout_content.className = "callout-content";
+				note_toolbar_callout_content.append(note_toolbar_ul);
+		
+				let note_toolbar_callout = document.createElement("div");
+				note_toolbar_callout.className = "callout dv-cg-note-toolbar";
+				note_toolbar_callout.setAttribute("tabindex", "0");
+				note_toolbar_callout.setAttribute("data-callout", "note-toolbar");
+				note_toolbar_callout.setAttribute("data-callout-metadata", "border-even-sticky");
+				note_toolbar_callout.setAttribute("data-updated", this.settings.updated);
+				note_toolbar_callout.append(note_toolbar_callout_content);
+		
+				/* workaround to emulate callout-in-content structure, to use same sticky css */
+				let div = document.createElement("div");
+				div.append(note_toolbar_callout);
+				let embed_block = document.createElement("div");
+				embed_block.className = "cm-embed-block cm-callout";
+				embed_block.append(div);
+		
+				/* inject it between the properties and content divs */
+				let properties_container = document.querySelector('.workspace-tab-container > .mod-active .metadata-container');
+				properties_container?.insertAdjacentElement("afterend", embed_block);
+		
+			}
+		} 
+		else {
+			this.remove_toolbar();
 		}
 	
 	}
