b9af9612f83ab06ae898d1a3a8ca4e870f389ec6
Chris Gurney <chris@chrisgurney.ca>
2024-03-13 17:13:27 -0400

    fix: multiple empty state message fragments not being displayed properly

--- src/Settings/NoteToolbarSettingTab.ts
+++ src/Settings/NoteToolbarSettingTab.ts
@@ -18,6 +18,14 @@ export class NoteToolbarSettingTab extends PluginSettingTab {
         modal.open();
     }
 
+	empty_message_fragment(text: string): DocumentFragment {
+		let message_fragment = document.createDocumentFragment();
+		let message_fragment_text = document.createElement("i")
+		message_fragment_text.textContent = text;
+		message_fragment.append(message_fragment_text);
+		return message_fragment;
+	}
+
 	public display(): void {
 
 		const { containerEl } = this;
@@ -27,26 +35,19 @@ export class NoteToolbarSettingTab extends PluginSettingTab {
 		containerEl.createEl("h2", { text: "Toolbars" });
 
 		if (this.plugin.settings.toolbars.length == 0) {
-			const message_no_toolbars = document.createDocumentFragment();
-			let message_no_toolbars_text = document.createElement("i")
-			message_no_toolbars_text.textContent = "Click the button to create a toolbar.";
-			message_no_toolbars.append(message_no_toolbars_text);	
 			containerEl
-				.createEl("div", { text: message_no_toolbars })
+				.createEl("div", { text: this.empty_message_fragment("Click the button to create a toolbar.") })
 				.className = "setting-item-name";
 		}
 		else {
-			const message_empty_items = document.createDocumentFragment();
-			let message_empty_items_text = document.createElement("i")
-			message_empty_items_text.textContent = "No toolbar items. Click Edit to update this toolbar.";
-			message_empty_items.append(message_empty_items_text);	
 			this.plugin.settings.toolbars.forEach(
 				(toolbar_item, index) => {
+					console.log(toolbar_item.name + " : " + toolbar_item.items.length);
 					new Setting(containerEl)
 						.setName(toolbar_item.name)
 						.setDesc(toolbar_item.items.length > 0 ? 
 							toolbar_item.items.map(item => item.label).join(' | ') : 
-							message_empty_items)
+							this.empty_message_fragment("No toolbar items. Click Edit to update this toolbar."))
 						.addButton((button: ButtonComponent) => {
 							button
 								.setTooltip("Update this toolbar's items")
