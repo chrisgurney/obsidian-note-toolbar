f8ce0518bfc104ac10bb959ca3d7124d37713afe
Chris Gurney <chris@chrisgurney.ca>
2024-03-13 15:56:06 -0400

    style: empty state messages

--- src/Settings/NoteToolbarSettingTab.ts
+++ src/Settings/NoteToolbarSettingTab.ts
@@ -27,16 +27,26 @@ export class NoteToolbarSettingTab extends PluginSettingTab {
 		containerEl.createEl("h2", { text: "Toolbars" });
 
 		if (this.plugin.settings.toolbars.length == 0) {
+			const message_no_toolbars = document.createDocumentFragment();
+			let message_no_toolbars_text = document.createElement("i")
+			message_no_toolbars_text.textContent = "Click the button to create a toolbar.";
+			message_no_toolbars.append(message_no_toolbars_text);	
 			containerEl
-				.createEl("div", { text: "No toolbars configured" })
+				.createEl("div", { text: message_no_toolbars })
 				.className = "setting-item-name";
 		}
 		else {
+			const message_empty_items = document.createDocumentFragment();
+			let message_empty_items_text = document.createElement("i")
+			message_empty_items_text.textContent = "No toolbar items. Click Edit to update this toolbar.";
+			message_empty_items.append(message_empty_items_text);	
 			this.plugin.settings.toolbars.forEach(
 				(toolbar_item, index) => {
 					new Setting(containerEl)
 						.setName(toolbar_item.name)
-						.setDesc(toolbar_item.items.length > 0 ? toolbar_item.items.map(item => item.label).join(' | ') : "No toolbar items")
+						.setDesc(toolbar_item.items.length > 0 ? 
+							toolbar_item.items.map(item => item.label).join(' | ') : 
+							message_empty_items)
 						.addButton((button: ButtonComponent) => {
 							button
 								.setTooltip("Update this toolbar's items")
