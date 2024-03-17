4aa9e666f6c5eaf335677392d79d935fc6ed48f5
Chris Gurney <chris@chrisgurney.ca>
2024-03-17 07:24:10 -0400

    refactor: move to javascript naming convention

--- src/Settings/ToolbarSettingsModal.ts
+++ src/Settings/ToolbarSettingsModal.ts
@@ -20,7 +20,7 @@ export default class ToolbarSettingsModal extends Modal {
 
 	onOpen() {
 		// const { contentEl } = this;
-		this.display_toolbar_settings();
+		this.displayToolbarSettings();
 	}
 
 	onClose() {
@@ -29,115 +29,123 @@ export default class ToolbarSettingsModal extends Modal {
 		this.parent.display();
 	}
 
-	public display_toolbar_settings() {
+	public displayToolbarSettings() {
 
 		this.modalEl.addClass("note-toolbar-setting-modal-container");
 
 		this.contentEl.empty();
 
-		let settings_div = this.containerEl.createEl("div");
-		settings_div.className = "vertical-tab-content note-toolbar-setting-modal";
+		let settingsDiv = this.containerEl.createEl("div");
+		settingsDiv.className = "vertical-tab-content note-toolbar-setting-modal";
 
-		const name_description = document.createDocumentFragment();
-		name_description.append(
+		//
+		// Toolbar name
+		// 
+
+		const nameDescription = document.createDocumentFragment();
+		nameDescription.append(
 			"Give this toolbar a unique name.",
-			name_description.createEl("br"),
+			nameDescription.createEl("br"),
 			"If a `notetoolbar` property is set to use this toolbar, it will take precedence over any folder toolbars."
 		);
 
-		let toolbar_name_div = this.containerEl.createEl("div");
-		new Setting(toolbar_name_div)
+		let toolbarNameDiv = this.containerEl.createEl("div");
+		new Setting(toolbarNameDiv)
 			.setName("Name")
-			.setDesc(name_description)
+			.setDesc(nameDescription)
 			.addText(text => text
 				.setPlaceholder('Name')
 				.setValue(this.toolbar.name)
 				.onChange(async (value) => {
 					// check for existing toolbar with this name
-					let existing_toolbar = this.plugin.get_toolbar_settings(value);
-					if (existing_toolbar && existing_toolbar !== this.toolbar) {
-						toolbar_name_div.createEl("div", { 
+					let existingToolbar = this.plugin.get_toolbar_settings(value);
+					if (existingToolbar && existingToolbar !== this.toolbar) {
+						toolbarNameDiv.createEl("div", { 
 							text: "A toolbar already exists with this name", 
 							attr: { id: "note-toolbar-name-error" }, cls: "note-toolbar-setting-error-message" });
-						toolbar_name_div.addClass("note-toolbar-setting-error");
+						toolbarNameDiv.addClass("note-toolbar-setting-error");
 					}
 					else {
 						document.getElementById("note-toolbar-name-error")?.remove();
-						toolbar_name_div.removeClass("note-toolbar-setting-error");
+						toolbarNameDiv.removeClass("note-toolbar-setting-error");
 						this.toolbar.name = value;
 						this.toolbar.updated = new Date().toISOString();
 						// console.log(this.toolbar);
 						await this.plugin.save_settings();	
 					}
 			}));
-		settings_div.append(toolbar_name_div);
+		settingsDiv.append(toolbarNameDiv);
+
+		//
+		// Item list
+		// 
 
 		// TODO: refactor to use variation of heading_fragment()
-		let item_list_heading = createEl("div");
-		item_list_heading.className = "note-toolbar-setting-items-header";
-		let item_list_title = createEl("div", { text: "Items" });
-		item_list_title.className = "setting-item-info";
-		let item_list_description = createEl("div", { text: "Items that appear in the toolbar, in order." });
-		item_list_description.className = "setting-item-description";
-		item_list_heading.append(item_list_title);
-		item_list_heading.append(item_list_description);
-		settings_div.append(item_list_heading);
+		let itemListHeading = createEl("div");
+		itemListHeading.className = "note-toolbar-setting-items-header";
+		let itemListTitle = createEl("div", { text: "Items" });
+		itemListTitle.className = "setting-item-info";
+		let itemListDesc = createEl("div", { text: "Items that appear in the toolbar, in order." });
+		itemListDesc.className = "setting-item-description";
+		itemListHeading.append(itemListTitle);
+		itemListHeading.append(itemListDesc);
+		settingsDiv.append(itemListHeading);
 
-		let last_item_index = 0;
+		let lastItemIndex = 0;
 		this.toolbar.items.forEach(
-			(toolbar_item, index) => {
-				last_item_index = index;
-				let item_div = this.containerEl.createEl("div");
-				item_div.className = "note-toolbar-setting-item";
-				let row1_container = this.containerEl.createEl("div");
-				row1_container.style.display = "flex";
-				row1_container.style.flexFlow = "wrap-reverse";
-				row1_container.style.justifyContent = "space-between";
+			(toolbarItem, index) => {
+				lastItemIndex = index;
+				let itemDiv = this.containerEl.createEl("div");
+				itemDiv.className = "note-toolbar-setting-item";
+				let row1Container = this.containerEl.createEl("div");
+				row1Container.style.display = "flex";
+				row1Container.style.flexFlow = "wrap-reverse";
+				row1Container.style.justifyContent = "space-between";
 
-				let text_fields_container = this.containerEl.createEl("div");
-				text_fields_container.id = "note-toolbar-setting-item-field-" + index;
-				text_fields_container.style.display = "flex";
-				text_fields_container.style.flexWrap = "wrap";
-				const s1a = new Setting(text_fields_container)
+				let textFieldsContainer = this.containerEl.createEl("div");
+				textFieldsContainer.id = "note-toolbar-setting-item-field-" + index;
+				textFieldsContainer.style.display = "flex";
+				textFieldsContainer.style.flexWrap = "wrap";
+				const s1a = new Setting(textFieldsContainer)
 					.setClass("note-toolbar-setting-item-field")
 					.addText(text => text
 						.setPlaceholder('Item label')
-						.setValue(toolbar_item.label)
+						.setValue(toolbarItem.label)
 						.onChange(async (value) => {
-							toolbar_item.label = value;
+							toolbarItem.label = value;
 							this.toolbar.updated = new Date().toISOString();
 							await this.plugin.save_settings();
 					}));
-				const s1c = new Setting(text_fields_container)
+				const s1c = new Setting(textFieldsContainer)
 					.setClass("note-toolbar-setting-item-field")
 					.addText(text => text
 						.setPlaceholder('Tooltip (optional)')
-						.setValue(toolbar_item.tooltip)
+						.setValue(toolbarItem.tooltip)
 						.onChange(async (value) => {
-							toolbar_item.tooltip = value;
+							toolbarItem.tooltip = value;
 							this.toolbar.updated = new Date().toISOString();
 							await this.plugin.save_settings();
 					}));
 
-				let text_fields_url_div = this.containerEl.createEl("div");
-				text_fields_url_div.style.display = "flex";
-				text_fields_url_div.style.flexWrap = "wrap";
-				text_fields_url_div.style.flexGrow = "1";
-				const s1b = new Setting(text_fields_url_div)
+				let textFieldsUrlDiv = this.containerEl.createEl("div");
+				textFieldsUrlDiv.style.display = "flex";
+				textFieldsUrlDiv.style.flexWrap = "wrap";
+				textFieldsUrlDiv.style.flexGrow = "1";
+				const s1b = new Setting(textFieldsUrlDiv)
 					.setClass("note-toolbar-setting-item-field-url")
 					.addText(text => text
 						.setPlaceholder('URL')
-						.setValue(toolbar_item.url)
+						.setValue(toolbarItem.url)
 						.onChange(async (value) => {
-							toolbar_item.url = value;
+							toolbarItem.url = value;
 							this.toolbar.updated = new Date().toISOString();
 							await this.plugin.save_settings();
 					}));
 
-				let item_controls_div = this.containerEl.createEl("div");
-				item_controls_div.style.marginLeft = "auto";
-				item_controls_div.className = "note-toolbar-setting-item-controls";
-				const s1d = new Setting(item_controls_div)
+				let itemControlsDiv = this.containerEl.createEl("div");
+				itemControlsDiv.style.marginLeft = "auto";
+				itemControlsDiv.className = "note-toolbar-setting-item-controls";
+				const s1d = new Setting(itemControlsDiv)
 					.addExtraButton((cb) => {
 						cb.setIcon("up-chevron-glyph")
 							.setTooltip("Move up")
@@ -149,7 +157,7 @@ export default class ToolbarSettingsModal extends Modal {
 								);
 								this.toolbar.updated = new Date().toISOString();
 								this.plugin.save_settings();
-								this.display_toolbar_settings();
+								this.displayToolbarSettings();
 							});
 					})
 					.addExtraButton((cb) => {
@@ -163,7 +171,7 @@ export default class ToolbarSettingsModal extends Modal {
 								);
 								this.toolbar.updated = new Date().toISOString();
 								this.plugin.save_settings();
-								this.display_toolbar_settings();
+								this.displayToolbarSettings();
 							});
 					})
 					.addExtraButton((cb) => {
@@ -176,51 +184,51 @@ export default class ToolbarSettingsModal extends Modal {
 								);
 								this.toolbar.updated = new Date().toISOString();
 								this.plugin.save_settings();
-								this.display_toolbar_settings();
+								this.displayToolbarSettings();
 							});
 					});
 
-				text_fields_container.appendChild(text_fields_url_div);
-				row1_container.appendChild(text_fields_container);
-				row1_container.appendChild(item_controls_div);
+				textFieldsContainer.appendChild(textFieldsUrlDiv);
+				row1Container.appendChild(textFieldsContainer);
+				row1Container.appendChild(itemControlsDiv);
 
-				item_div.appendChild(row1_container);
+				itemDiv.appendChild(row1Container);
 
-				let toggles_div = this.containerEl.createEl("div");
-				toggles_div.style.display = "flex";
-				toggles_div.style.justifyContent = "flex-end";
-				toggles_div.style.flexWrap = "wrap";
-				const s2 = new Setting(toggles_div)
+				let togglesDiv = this.containerEl.createEl("div");
+				togglesDiv.style.display = "flex";
+				togglesDiv.style.justifyContent = "flex-end";
+				togglesDiv.style.flexWrap = "wrap";
+				const s2 = new Setting(togglesDiv)
 					.setClass("note-toolbar-setting-item-toggle")
 					.setName("Hide on mobile")
 					.addToggle((toggle) => {
 						toggle
 							.setTooltip(('If enabled, this item will not appear on mobile'))
-							.setValue(toolbar_item.hide_on_mobile)
-							.onChange((hide_on_mobile) => {
-								toolbar_item.hide_on_mobile = hide_on_mobile;
+							.setValue(toolbarItem.hide_on_mobile)
+							.onChange((hideOnMobile) => {
+								toolbarItem.hide_on_mobile = hideOnMobile;
 								this.toolbar.updated = new Date().toISOString();
 								this.plugin.save_settings();
 							});
 					});
-				const s3 = new Setting(toggles_div)
+				const s3 = new Setting(togglesDiv)
 					.setClass("note-toolbar-setting-item-toggle")
 					.setName("Hide on desktop")
 					.addToggle((toggle) => {
 						toggle
 							.setTooltip(('If enabled, this item will not appear on desktop'))
-							.setValue(toolbar_item.hide_on_desktop)
-							.onChange((hide_on_desktop) => {
-								toolbar_item.hide_on_desktop = hide_on_desktop;
+							.setValue(toolbarItem.hide_on_desktop)
+							.onChange((hideOnDesktop) => {
+								toolbarItem.hide_on_desktop = hideOnDesktop;
 								this.toolbar.updated = new Date().toISOString();
 								this.plugin.save_settings();
 							});
 					});
-				item_div.appendChild(toggles_div);
-				settings_div.appendChild(item_div);
+				itemDiv.appendChild(togglesDiv);
+				settingsDiv.appendChild(itemDiv);
 			});
 
-		new Setting(settings_div)
+		new Setting(settingsDiv)
 			.setClass("note-toolbar-setting-button")
 			.addButton((button: ButtonComponent) => {
 				button
@@ -237,11 +245,15 @@ export default class ToolbarSettingsModal extends Modal {
 						});
 						this.toolbar.updated = new Date().toISOString();
 						this.plugin.save_settings();
-						this.display_toolbar_settings();
+						this.displayToolbarSettings();
 					});
 			});
 
-		new Setting(settings_div)
+		//
+		// Delete toolbar
+		// 
+
+		new Setting(settingsDiv)
 			.setName("Delete this toolbar")
 			.setDesc("This action cannot be undone")
 			.setClass("note-toolbar-setting-item-delete-button")
@@ -257,12 +269,12 @@ export default class ToolbarSettingsModal extends Modal {
 					});
 			});
 
-		this.contentEl.appendChild(settings_div);
+		this.contentEl.appendChild(settingsDiv);
 
 		// set focus on last thing in the list, if the label is empty
-		let input_to_focus = this.contentEl.querySelector('#note-toolbar-setting-item-field-' + last_item_index + ' input[type="text"]') as HTMLInputElement;
-		if (input_to_focus?.value.length === 0) {
-			input_to_focus.focus();
+		let inputToFocus = this.contentEl.querySelector('#note-toolbar-setting-item-field-' + lastItemIndex + ' input[type="text"]') as HTMLInputElement;
+		if (inputToFocus?.value.length === 0) {
+			inputToFocus.focus();
 		}
 
 	}
