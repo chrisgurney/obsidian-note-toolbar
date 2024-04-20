dcd00289ab94696d652252fc5eaaa9aacd239444
Chris Gurney <chris@chrisgurney.ca>
2024-04-20 17:25:41 -0400

    feat: add suggestions for * and / to folder mapping

--- src/Settings/Suggesters/FolderSuggester.ts
+++ src/Settings/Suggesters/FolderSuggester.ts
@@ -1,35 +1,63 @@
 import { AbstractInputSuggest, App, TAbstractFile, TFolder } from "obsidian";
 
-export class FolderSuggester extends AbstractInputSuggest<TFolder> {
+module FolderSuggester {
+    export type Pattern = {
+        pattern: string,
+        desc: string
+    }
+}
+
+export class FolderSuggester extends AbstractInputSuggest<string> {
 
     private inputEl: HTMLInputElement;
+
+    private PATTERN_ALL_FILES: FolderSuggester.Pattern = { pattern: '*', desc: 'all files' };
+    private PATTERN_ROOT_ONLY: FolderSuggester.Pattern = { pattern: '/', desc: 'vault root only' };
 
     constructor(app: App, inputEl: HTMLInputElement) {
         super(app, inputEl);
         this.inputEl = inputEl;
     }
 
-    getSuggestions(inputStr: string): TFolder[] {
+    getSuggestions(inputStr: string): Array<string> {
         const abstractFiles = this.app.vault.getAllLoadedFiles();
-        const folders: TFolder[] = [];
+        const folders: string[] = [];
         const lowerCaseInputStr = inputStr.toLowerCase();
+
+        folders.push(this.PATTERN_ALL_FILES.pattern);
+        // no need to add PATTERN_ROOT as it's included below
 
         abstractFiles.forEach((folder: TAbstractFile) => {
             if (folder instanceof TFolder && folder.path.toLowerCase().contains(lowerCaseInputStr)) {
-                folders.push(folder);
+                folders.push(folder.path);
             }
         });
 
         return folders;
     }
 
-    renderSuggestion(file: TFolder, el: HTMLElement): void {
-        el.setText(file.path);
+    renderSuggestion(folder: string, el: HTMLElement): void {
+        if (folder === this.PATTERN_ALL_FILES.pattern) {
+            this.renderPattern(el, this.PATTERN_ALL_FILES);
+        } 
+        else if (folder === this.PATTERN_ROOT_ONLY.pattern) {
+            this.renderPattern(el, this.PATTERN_ROOT_ONLY);
+        }
+        else {
+            el.setText(folder);
+        }
     }
 
-    selectSuggestion(file: TFolder): void {
-        this.inputEl.value = file.path;
+    selectSuggestion(folder: string): void {
+        this.inputEl.value = folder;
         this.inputEl.trigger("input");
         this.close();
     }
+
+    renderPattern(el: HTMLElement, suggestion: FolderSuggester.Pattern) {
+        el.addClass('note-toolbar-setting-folder-suggestion-item-muted');
+        el.createSpan().setText(suggestion.pattern);
+        el.createSpan().setText(suggestion.desc);
+    }
+
 }
