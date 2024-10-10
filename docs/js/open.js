1606e09896220a1854f66e3c0acc1d6c34d42535
Chris Gurney <chris@chrisgurney.ca>
2024-10-10 14:53:23 -0400

    chore: revert: improve validation and error logging

--- docs/js/open.js
+++ docs/js/open.js
@@ -1,19 +1,5 @@
 const uri = new URLSearchParams(window.location.search).get("uri");
-
-if (uri) {
-    try {
-        const parsedUrl = new URL(uri);
-        if (parsedUrl.protocol === "obsidian:" && parsedUrl.hostname === "note-toolbar") {
-            const param = parsedUrl.searchParams.get("import");
-            if (param) {
-                window.location.replace(`obsidian://note-toolbar?import=${encodeURIComponent(param)}`);
-            } else {
-                console.error("Missing 'import' parameter.");
-            }
-        } else {
-            console.error("Invalid protocol or host.");
-        }
-    } catch (e) {
-        console.error("Invalid URL format.");
-    }
+if (uri && uri.startsWith("obsidian://note-toolbar")) {
+    const [baseUri, param] = uri.split("?import=");
+    window.location.replace("obsidian://note-toolbar?import=" + encodeURIComponent(param));
 }
