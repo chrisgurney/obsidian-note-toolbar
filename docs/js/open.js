d1c1cbdf4dc866d1ec30806f679009783fcbeebe
Chris Gurney <chris@chrisgurney.ca>
2024-10-10 14:31:42 -0400

    fix: (test) remove unneeded encoding

--- docs/js/open.js
+++ docs/js/open.js
@@ -1,4 +1,4 @@
 const uri = new URLSearchParams(window.location.search).get("uri");
 if (uri && uri.startsWith("obsidian://note-toolbar")) {
-    window.location.replace(encodeURI(uri));
+    window.location.replace(uri);
 }
