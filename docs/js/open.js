ac2cd79d54c73de989b841be76b0bb802af287d2
Chris Gurney <chris@chrisgurney.ca>
2024-10-14 17:21:45 -0400

    fix: URI not redirecting

--- docs/js/open.js
+++ docs/js/open.js
@@ -1,7 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const uri = new URLSearchParams(window.location.search).get("uri");
-    if (uri && uri.startsWith("obsidian://note-toolbar")) {
-        const [baseUri, param] = uri.split("?import=");
-        window.location.replace("obsidian://note-toolbar?import=" + encodeURIComponent(param));
-    }
-});
+const uri = new URLSearchParams(window.location.search).get("uri");
+if (uri && uri.startsWith("obsidian://note-toolbar")) {
+    const [baseUri, param] = uri.split("?import=");
+    window.location.replace("obsidian://note-toolbar?import=" + encodeURIComponent(param));
+}
