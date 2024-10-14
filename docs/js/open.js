47ba353e15374f44798614abd3f9772f59bda99c
Chris Gurney <chris@chrisgurney.ca>
2024-10-14 17:26:10 -0400

    feat: display message after URI loads, since the tab is left open

--- docs/js/open.js
+++ docs/js/open.js
@@ -1,5 +1,7 @@
-const uri = new URLSearchParams(window.location.search).get("uri");
-if (uri && uri.startsWith("obsidian://note-toolbar")) {
-    const [baseUri, param] = uri.split("?import=");
-    window.location.replace("obsidian://note-toolbar?import=" + encodeURIComponent(param));
-}
+document.addEventListener("DOMContentLoaded", () => {
+    const uri = new URLSearchParams(window.location.search).get("uri");
+    if (uri && uri.startsWith("obsidian://note-toolbar")) {
+        const [baseUri, param] = uri.split("?import=");
+        window.location.replace("obsidian://note-toolbar?import=" + encodeURIComponent(param));
+    }
+});
