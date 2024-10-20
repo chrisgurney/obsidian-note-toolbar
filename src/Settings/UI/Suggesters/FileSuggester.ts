38851f1ceda6d6ea3dda456f4fce795a5303972c
Chris Gurney <chris@chrisgurney.ca>
2024-10-20 15:49:41 -0400

    chore: option to limit FileSelector to folders and file extensions

--- src/Settings/UI/Suggesters/FileSuggester.ts
+++ src/Settings/UI/Suggesters/FileSuggester.ts
@@ -4,11 +4,15 @@ export class FileSuggester extends AbstractInputSuggest<TAbstractFile> {
 
     private inputEl: HTMLInputElement;
     private showFilesOnly: boolean;
+    private fileExtension: string | undefined;
+    private folderPath: string | undefined;
 
-    constructor(app: App, inputEl: HTMLInputElement, showFilesOnly: boolean = false) {
+    constructor(app: App, inputEl: HTMLInputElement, showFilesOnly: boolean = false, fileExtension?: string, folderPath?: string) {
         super(app, inputEl);
         this.inputEl = inputEl;
         this.showFilesOnly = showFilesOnly;
+        this.fileExtension = fileExtension;
+        this.folderPath = folderPath;
     }
 
     getSuggestions(inputStr: string): TAbstractFile[] {
@@ -16,10 +20,15 @@ export class FileSuggester extends AbstractInputSuggest<TAbstractFile> {
         let files: TAbstractFile[] = [];
         const lowerCaseInputStr = inputStr.toLowerCase();
 
-        files = abstractFiles.filter((file: TAbstractFile) =>
-            (file instanceof TFile || (!this.showFilesOnly && file instanceof TFolder)) && 
-            file.path.toLowerCase().includes(lowerCaseInputStr)
-        );
+        files = abstractFiles.filter((file: TAbstractFile) => {
+            const isFile = file instanceof TFile;
+            let matchesInput = file.path.toLowerCase().includes(lowerCaseInputStr);
+            if (!matchesInput) return false;
+            if (this.showFilesOnly && !isFile) return false;
+            if (this.fileExtension && isFile && !file.path.endsWith(this.fileExtension)) return false;
+            if (this.folderPath && !file.path.startsWith(this.folderPath)) return false;
+            return true;
+        });
 
         return files;
     }
