ca5adf2fdf1a6bc2a1d3640c09688dc82362a3fd
Chris Gurney <chris@chrisgurney.ca>
2024-12-31 09:20:16 -0500

    docs: documentation for suggester() params.

--- src/Api/NoteToolbarApi.ts
+++ src/Api/NoteToolbarApi.ts
@@ -6,6 +6,14 @@ type Callback = (arg: string) => void;
 
 export interface INoteToolbarApi<T> {
     testCallback: (buttonId: string, callback: Callback) => Promise<void>;
+    /**
+     * @param text_items Array of strings representing the text that will be displayed for each item in the suggester prompt. This can also be a function that maps an item to its text representation.
+     * @param items Array containing the values of each item in the correct order.
+     * @param throw_on_cancel Throws an error if the prompt is canceled, instead of returning a `null` value.
+     * @param placeholder Placeholder string of the prompt.
+     * @param limit Limit the number of items rendered at once (useful to improve performance when displaying large lists).
+     * @returns The user's chosen item.
+     */
     suggester: (text_items: string[] | ((item: T) => string), items: T[], throw_on_cancel: boolean, placeholder: string, limit?: number) => Promise<T | null>;
 }
 
