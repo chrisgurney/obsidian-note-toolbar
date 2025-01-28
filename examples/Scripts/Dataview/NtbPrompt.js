4afe3f0c1bd9de2c36b24e0f30f2403d60f718cc
Chris Gurney <chris@chrisgurney.ca>
2025-01-28 09:27:42 -0500

    docs: updated API examples for shortened prompt() params

--- examples/Scripts/Dataview/NtbPrompt.js
+++ examples/Scripts/Dataview/NtbPrompt.js
@@ -17,35 +17,35 @@
 
     // same as previous, with text above input field added
     const result2 = await NoteToolbar.prompt({
-        prompt_text: "Enter some text"
+        label: "Enter some text"
     });
     new Notice(result2);
 
     // same as previous, with default placeholder text overridden 
     const result3 = await NoteToolbar.prompt({
-        prompt_text: "Enter some text",
+        label: "Enter some text",
         placeholder: ""
     });
     new Notice(result3);
 
     // default (one-line) prompt with message, overridden placeholder, and default value 
     const result4 = await NoteToolbar.prompt({
-        prompt_text: "Enter some text",
+        label: "Enter some text",
         placeholder: "Placeholder",
-        default_value: "Default"
+        default: "Default"
     });
     new Notice(result4);
 
     // multi-line prompt
     const result5 = await NoteToolbar.prompt({
-        multi_line: true
+        large: true
     });
     new Notice(result5);
 
     // same as previous, but with text above input field (markdown is supported)
     const result6 = await NoteToolbar.prompt({
-        multi_line: true,
-        prompt_text: "Enter _lots_ of text:"
+        large: true,
+        label: "Enter _lots_ of text:"
     });
     new Notice(result6);
     
