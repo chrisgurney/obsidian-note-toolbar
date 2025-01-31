7cd92f70e242a7ba3fed0bcf9256fd0d902a28a2
Chris Gurney <chris@chrisgurney.ca>
2025-01-31 08:56:02 -0500

    refactor: API UI CSS for making UI compact

--- src/Api/NtbPrompt.ts
+++ src/Api/NtbPrompt.ts
@@ -49,7 +49,7 @@ export class NtbPrompt extends Modal {
         this.default = default_value;
 
         this.modalEl.addClasses(['prompt', 'note-toolbar-ui']);
-        if (!this.label) this.modalEl.setAttr('data-ntb-ui-mode', 'compact');
+        if (!this.label) this.modalEl.setAttr('data-ntb-ui-mode', 'noclose-noheader');
     }
 
     onOpen(): void {
