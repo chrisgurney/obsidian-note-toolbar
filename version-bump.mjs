2ede79dcac53ae0b57d7254dfe570779fe39862d
Chris Gurney <chris@chrisgurney.ca>
2025-04-30 10:57:54 -0400

    build: (undo) updated version script

--- version-bump.mjs
+++ version-bump.mjs
@@ -8,11 +8,6 @@ const { minAppVersion } = manifest;
 manifest.version = targetVersion;
 writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
 
-// read minAppVersion from manifest.json and bump version to target version
-let packageJson = JSON.parse(readFileSync("package.json", "utf8"));
-packageJson.version = targetVersion;
-writeFileSync("package.json", JSON.stringify(packageJson, null, "\t"));
-
 // update versions.json with target version and minAppVersion from manifest.json
 let versions = JSON.parse(readFileSync("versions.json", "utf8"));
 versions[targetVersion] = minAppVersion;
