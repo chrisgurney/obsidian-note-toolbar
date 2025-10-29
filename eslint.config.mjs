3196a3bcb49b1a139f499e98ae8d4886b834b947
Chris Gurney <chris@chrisgurney.ca>
2025-10-29 12:44:55 -0400

    build: updated eslint, added Obsidian eslint plugin

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import obsidianmd from "eslint-plugin-obsidianmd";
+
+import globals from "globals";
+import { defineConfig, globalIgnores } from "eslint/config";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default defineConfig([
+    globalIgnores([
+        "build/*",
+        "examples/*",
+        "main.js",
+        "*.mjs",
+    ]),
+    {
+        files: [ "**/*.ts" ],
+        languageOptions: {
+            globals: {
+				...globals.node,
+			},
+            parser: tsParser,
+            parserOptions: {
+                project: "./tsconfig.json",
+                sourceType: "module"
+            },
+        },
+        plugins: { 
+            obsidianmd,
+            "@typescript-eslint": tsPlugin
+        },
+    },
+    // bring in Obsidian's recommended rules
+    ...obsidianmd.configs.recommended,
+    // ignore some basic rules for now
+    {
+        rules: {
+            'no-undef': 'off',
+            ...Object.fromEntries(
+                Object.keys(tsPlugin.rules).map(r => [`@typescript-eslint/${r}`, "off"])
+            )
+        }
+    }
+]);
