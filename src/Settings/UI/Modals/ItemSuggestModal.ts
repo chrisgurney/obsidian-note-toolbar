e1443f3d33899b5f14c184278d2e9e8eb49dfba9
Chris Gurney <chris@chrisgurney.ca>
2025-12-02 12:01:37 -0500

    fix: item suggester was not showing gallery matches if there were no existing item matches

--- src/Settings/UI/Modals/ItemSuggestModal.ts
+++ src/Settings/UI/Modals/ItemSuggestModal.ts
@@ -149,21 +149,21 @@ export default class ItemSuggestModal extends SuggestModal<ToolbarItemSettings> 
             }
         }
 
-        if (this.hasResults) {
-            if (this.mode !== 'QuickTools' && !isInputEmpty) {
-                // add gallery items
-                let gallerySuggestions: ToolbarItemSettings[] = [];
-                for (const galleryItem of this.ntb.gallery.getItems()) {
-                    if (await this.isSearchMatch(galleryItem, lowerCaseInputStr)) gallerySuggestions.push(galleryItem);
-                }
-                gallerySuggestions = this.sortSuggestions(gallerySuggestions, lowerCaseInputStr);
-                if (gallerySuggestions.length > 0) {
-                    sortedSuggestions.push(ITEM_GALLERY_DIVIDER);
-                    sortedSuggestions.push(...gallerySuggestions);
-                }
+        // add matching gallery items
+        if (this.mode !== 'QuickTools' && !isInputEmpty) {
+            let gallerySuggestions: ToolbarItemSettings[] = [];
+            for (const galleryItem of this.ntb.gallery.getItems()) {
+                if (await this.isSearchMatch(galleryItem, lowerCaseInputStr)) gallerySuggestions.push(galleryItem);
+            }
+            gallerySuggestions = this.sortSuggestions(gallerySuggestions, lowerCaseInputStr);
+            if (gallerySuggestions.length > 0) {
+                sortedSuggestions.push(ITEM_GALLERY_DIVIDER);
+                sortedSuggestions.push(...gallerySuggestions);
+                this.hasResults = true;
             }
         }
-        else {
+
+        if (!this.hasResults) {
             sortedSuggestions = [this.NEW_ITEM];
         }
 
