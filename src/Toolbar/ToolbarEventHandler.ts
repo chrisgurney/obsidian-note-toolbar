c83c16c2a2f1423dee352d8c30d62e2e7c4eed57
Chris Gurney <chris@chrisgurney.ca>
2025-12-02 09:59:34 -0500

    fix: if current toolbar is same as text toolbar, Position option does not show in context menu

--- src/Toolbar/ToolbarEventHandler.ts
+++ src/Toolbar/ToolbarEventHandler.ts
@@ -7,6 +7,7 @@ import StyleModal from "Settings/UI/Modals/StyleModal";
 import ToolbarSettingsModal from "Settings/UI/Modals/ToolbarSettingsModal";
 import { openItemSuggestModal, learnMoreFr } from "Settings/UI/Utils/SettingsUIUtils";
 import { exportToCallout, importFromCallout } from "Utils/ImportExport";
+import { TbarData } from "./ToolbarRenderer";
 
 /**
  * Handles toolbar events registered with Obsidian's `registerEvent()`. 
@@ -68,6 +69,7 @@ export default class ToolbarEventHandler {
 		// figure out what toolbar we're in
 		let toolbarEl = (mouseEvent.target as Element).closest('.cg-note-toolbar-container') as HTMLElement;
 		let toolbarSettings = toolbarEl?.id ? this.ntb.settingsManager.getToolbarById(toolbarEl.id) : undefined;
+		const isTextToolbar = toolbarEl.getAttribute(TbarData.Position) === PositionType.Text;
 
 		// figure out what item was clicked on (if any)
 		let toolbarItemEl: Element | null = null;
@@ -83,7 +85,6 @@ export default class ToolbarEventHandler {
 
 		const currentView = this.ntb.app.workspace.getActiveViewOfType(ItemView);
 		const currentPosition = toolbarSettings ? this.ntb.settingsManager.getToolbarPosition(toolbarSettings) : undefined;
-		const isTextToolbar = toolbarSettings ? (this.ntb.settings.textToolbar === toolbarSettings.uuid) : false;
 
 		if (toolbarSettings !== undefined) {
 
