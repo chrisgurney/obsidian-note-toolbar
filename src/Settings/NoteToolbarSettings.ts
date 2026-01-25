33e72f091ad2c33feb6213426716e574cd7ef292
Chris Gurney <chris@chrisgurney.ca>
2026-01-25 08:11:39 -0500

    chore: updated What's New version

--- src/Settings/NoteToolbarSettings.ts
+++ src/Settings/NoteToolbarSettings.ts
@@ -2,7 +2,7 @@ import { getUUID } from "Utils/Utils";
 import { getLanguage, PaneType } from "obsidian";
 
 /* updates link to plugin's release notes and displays What's New view */
-export const WHATSNEW_VERSION = '1.27';
+export const WHATSNEW_VERSION = '1.28';
 
 /* only update when settings structure changes to trigger migrations */
 export const SETTINGS_VERSION = 20260122.1;
