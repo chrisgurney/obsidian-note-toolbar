911a460206e04e277c0453aa32b726ca2d616696
Chris Gurney <chris@chrisgurney.ca>
2026-02-04 13:25:47 -0500

    docs: Dataview JS example that shows bookmarks as a scrollable list of cards

---
+++ examples/Scripts/Dataview/BookmarkCards.js
@@ -0,0 +1,32 @@
+/* Shows a bookmarks as a scrollable list of cards.
+
+TO USE: 
+- In a note:
+ ```dataviewjs
+ await dv.view("path/to/BookmarkCards")
+ ```
+*/
+
+function BookmarkCards() {
+    // list bookmarks
+    // const bm = ntb.app.internalPlugins.plugins['bookmarks'];
+    const bm = app.internalPlugins.plugins['bookmarks'];
+    const i = bm.instance?.getBookmarks();
+    const mi = i
+    .filter(b => b.type === 'file' || b.type === 'folder')
+    .map(b => ({
+        value: b.path,
+        label: b.title ? b.title : b.path,
+        icon: b.type === 'folder' ? 'LiFolder' : 'LiFile'
+    }));
+
+    // build the callout
+    let callout = '> [!note-toolbar|card-nowrap-red]\n';
+    mi.forEach(item => {
+        callout += `> - [[${item.value}|:${item.icon}: ${item.label}]]\n`;
+    });
+
+    dv.span(callout);
+}
+
+BookmarkCards()
