9d2d8fa50eac6bf9012c6993c60a2f60a8c4302c
Chris Gurney <chris@chrisgurney.ca>
2026-04-10 15:23:09 -0400

    build: tried eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -51,13 +51,12 @@ export default defineConfig([
 			"no-debugger": "warn",
 			
 			"@typescript-eslint/no-invalid-this": "error",
+			// "@typescript-eslint/no-non-null-assertion": "error",
+			// "@typescript-eslint/no-unused-vars": "error",
 
 			// for support of older versions of iOS (15 and earlier)
 			"es-x/no-regexp-lookbehind-assertions": "error",
 
-			// "@typescript-eslint/no-non-null-assertion": "error",
-			// "@typescript-eslint/no-unused-vars": "error",
-			
 			// COMMENT BACK IN BELOW SUGGESTED LIST TO TACKLE ONE-BY-ONE
 
 			// MEMORY
