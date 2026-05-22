3b7a559b8b1aca86bcea5419f3de86e6dc9a8702
Chris Gurney <chris@chrisgurney.ca>
2026-05-22 06:58:21 -0400

    fix: review: lint errors #546

--- src/Toolbar/RibbonMenu.ts
+++ src/Toolbar/RibbonMenu.ts
@@ -10,17 +10,17 @@ export default class RibbonMenu {
 
     async render(event: MouseEvent) {
 
-        switch (this.ntb.settings.ribbonAction as RibbonAction) {
+        switch (this.ntb.settings.ribbonAction) {
             case (RibbonAction.ItemSuggester): {
-                await this.ntb.commands.openQuickTools();
+                this.ntb.commands.openQuickTools();
                 break;
             }
             case (RibbonAction.ToolbarSelected): {
                 if (this.ntb.settings.ribbonToolbar) {
-                    let toolbar = this.ntb.settingsManager.getToolbarById(this.ntb.settings.ribbonToolbar);
-                    let activeFile = this.ntb.app.workspace.getActiveFile();
+                    const toolbar = this.ntb.settingsManager.getToolbarById(this.ntb.settings.ribbonToolbar);
+                    const activeFile = this.ntb.app.workspace.getActiveFile();
                     if (toolbar && activeFile) {
-                        this.ntb.render.renderAsMenu(toolbar, activeFile, this.ntb.settings.showEditInFabMenu).then(menu => { 
+                        await this.ntb.render.renderAsMenu(toolbar, activeFile, this.ntb.settings.showEditInFabMenu).then(menu => { 
                             menu.showAtPosition(event); 
                         });
                     }
@@ -28,16 +28,16 @@ export default class RibbonMenu {
                 break;
             }
             case (RibbonAction.ToolbarSuggester): {
-                await this.ntb.commands.openToolbarSuggester();
+                this.ntb.commands.openToolbarSuggester();
                 break;
             }
             case (RibbonAction.Toolbar): {
-                let activeFile = this.ntb.app.workspace.getActiveFile();
+                const activeFile = this.ntb.app.workspace.getActiveFile();
                 if (activeFile) {
-                    let frontmatter = activeFile ? this.ntb.app.metadataCache.getFileCache(activeFile)?.frontmatter : undefined;
-                    let toolbar: ToolbarSettings | undefined = this.ntb.settingsManager.getMappedToolbar(frontmatter, activeFile);
+                    const frontmatter = activeFile ? this.ntb.app.metadataCache.getFileCache(activeFile)?.frontmatter : undefined;
+                    const toolbar: ToolbarSettings | undefined = this.ntb.settingsManager.getMappedToolbar(frontmatter, activeFile);
                     if (toolbar) {
-                        this.ntb.render.renderAsMenu(toolbar, activeFile, this.ntb.settings.showEditInFabMenu).then(menu => { 
+                        await this.ntb.render.renderAsMenu(toolbar, activeFile, this.ntb.settings.showEditInFabMenu).then(menu => { 
                             menu.showAtPosition(event); 
                         });
                     }
