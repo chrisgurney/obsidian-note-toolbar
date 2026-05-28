d2728bd661285cb3d0ef1ff0d277c756d6b27919
Chris Gurney <chris@chrisgurney.ca>
2026-05-28 14:38:50 -0400

    fix: remove use of CSS !has #546

--- src/Toolbar/CalloutPlugin.ts
+++ src/Toolbar/CalloutPlugin.ts
@@ -39,7 +39,7 @@ export class CalloutPluginClass implements PluginValue {
     process(view: EditorView): void {
         // timeout lets CodeMirror finish updating the DOM before querying it
         activeWindow.setTimeout(() => {
-            this.ntb.debug('Processing callouts...');
+            // this.ntb.debug('Processing callouts...');
             view.dom.querySelectorAll('.cm-embed-block').forEach((el) => {
                 const toolbarEl = el.querySelector('.callout[data-callout="note-toolbar"]');
                 el.toggleAttribute(TbarData.EmbedIsNoteToolbar, !!toolbarEl);
