6b61f1b2c44667b303ff3db296e27acffc20d3f0
Chris Gurney <chris@chrisgurney.ca>
2026-06-11 15:42:07 -0400

    feat: fallback link for Obsidian Web Viewer

--- docs/js/open.js
+++ docs/js/open.js
@@ -2,15 +2,38 @@
  * This script opens links shared from Note Toolbar directly in Obsidian.
  * Learn more: https://github.com/chrisgurney/obsidian-note-toolbar/wiki/Sharing-toolbars
  *
- * What It Does:
- * 1. Reads a special link passed into the page (from the URL query string).
- * 2. Gets the Note Toolbar Callout value from the "import" parameter in the link.
- * 3. Redirects the browser to Obsidian.
+ * Flow:
+ * 1. read incoming uri from query string
+ * 2. extract import value
+ * 3. always render fallback link (needed for web viewers)
+ * 4. attempt redirect into obsidian
  */
-document.addEventListener("DOMContentLoaded", () => {
-    const uri = new URLSearchParams(window.location.search).get("uri");
-    if (uri && uri.startsWith("obsidian://note-toolbar")) {
-        const [baseUri, param] = uri.split("?import=");
-        window.location.replace("obsidian://note-toolbar?import=" + encodeURIComponent(param));
-    }
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	const uri = new URLSearchParams(window.location.search).get('uri');
+
+    // show a fallback link (Obsidian web viewer doesn't seem to do redirects)
+    const container = document.createElement('div');
+
+    const link = document.createElement('a');
+    link.style.display = 'block';
+    link.textContent = 'Open in Obsidian ↗';
+    link.href = 'obsidian://note-toolbar';
+
+    container.appendChild(link);
+    document.body.appendChild(container);
+
+    // ignore invalid input
+    if (!uri || !uri.startsWith('obsidian://note-toolbar')) return;
+
+    // extract import value from uri
+    const param = uri.split('?import=')[1];
+    if (!param) return;
+
+    const finalUrl = 'obsidian://note-toolbar?import=' + encodeURIComponent(param);
+
+    // update page link and attempt redirect
+    link.href = finalUrl;
+	window.location.replace(finalUrl);
 });
