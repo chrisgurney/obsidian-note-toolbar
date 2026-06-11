358ae67d093b23f94f27a3b1b2bfc566d888a860
Chris Gurney <chris@chrisgurney.ca>
2026-06-11 14:41:29 -0400

    docs: explain what this script does

--- docs/js/open.js
+++ docs/js/open.js
@@ -1,3 +1,11 @@
+/**
+ * This page opens links shared from Note Toolbar directly in Obsidian.
+ *
+ * What It Does:
+ * 1. Reads a special link passed into the page (from the URL query string).
+ * 2. Gets the Note Toolbar Callout value from the "import" parameter in the link.
+ * 3. Redirects the browser to Obsidian.
+ */
 document.addEventListener("DOMContentLoaded", () => {
     const uri = new URLSearchParams(window.location.search).get("uri");
     if (uri && uri.startsWith("obsidian://note-toolbar")) {
