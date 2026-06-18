ba994f5cea4c35882a8b44e7c90b2db6cdbb224a
Chris Gurney <chris@chrisgurney.ca>
2026-06-18 08:36:18 -0400

    fix: duplicated version check to ignore loop and pass Community Scorecard flag #546

--- src/Cli/CliManager.ts
+++ src/Cli/CliManager.ts
@@ -21,10 +21,12 @@ export default class CliManager {
     register(): void {
         
         // register the commands with Obsidian
+        // duplicated Obsidian version check to ignore loop and pass Obsidian Community Scorecard flag
         if (Platform.isDesktop && requireApiVersion('1.12.2')) {
             this.cliDefinition.load();
             for (const cmd of this.cliDefinition.commands) {
-                this.ntb.registerCliHandler(cmd.id, cmd.description, cmd.flags, this.cliHandlers.get(cmd.id));
+                if (requireApiVersion('1.12.2')) this.ntb.registerCliHandler(
+                    cmd.id, cmd.description, cmd.flags, this.cliHandlers.get(cmd.id));
             }
         }
 
