c4ace118b672d04527d64599082d53c4a9fd7557
Chris Gurney <chris@chrisgurney.ca>
2026-07-09 17:00:49 -0400

    fix: floating toolbars + keyboard: menus appearing in wrong location

--- src/Toolbar/ToolbarItemHandler.ts
+++ src/Toolbar/ToolbarItemHandler.ts
@@ -141,7 +141,7 @@ export default class ToolbarItemHandler {
                 const toolbar = this.ntb.settingsManager.getToolbar(linkHref);
                 if (toolbar) {
                     await this.ntb.render.renderAsMenu(toolbar, activeFile).then(menu => {
-                        const position = (!event || event instanceof KeyboardEvent) ? this.ntb.utils.getPosition('cursor') : this.ntb.render.lastClickedPos; 
+                        const position = !event ? this.ntb.utils.getPosition('cursor') : this.ntb.render.lastClickedPos; 
                         if (position) {
                             this.ntb.render.showMenuAtPosition(menu,
                                 { x: position.left, y: position.bottom, overlap: true, left: false }
